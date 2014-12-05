67022c75e91ce22f79b7c79194f4ca55e21bd189
Will Richman <williamrichman@gmail.com>
2014-12-05 13:55:42 -0800

    Styled the request form

--- packages/telescope-static-pages/lib/client/templates/request_submit.js
+++ packages/telescope-static-pages/lib/client/templates/request_submit.js
@@ -11,7 +11,6 @@ Template.requestSubmit.events({
     
     request._id = Requests.insert(request);
     var subject = "Welcome to Telescope, " + request.name;
-    debugger
     Meteor.call('buildAndSendEmail', request.email, subject, 'emailNewRequest', request)
     Router.go('talent', request);
   }
